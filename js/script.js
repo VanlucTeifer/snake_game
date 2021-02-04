19334034c191a94a93415465d3f5e7a648a946e7
Lucas Vanderlei <lucasvanderlei.f@gmail.com>
2021-02-04 09:08:05 -0300

    correcao de script

--- js/script.js
+++ js/script.js
@@ -41,16 +41,17 @@ function iniciarJogo(){
   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
   
-  for(i = 1; i < snake.length; i++){
-    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-      clearInterval(jogo);
-      alert("Game Over :(")
-    }
-  }
-
   criaBG();
   criarSnake();
   drawFood();
+  
+  for(i = 1; i < snake.length; i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+      clearInterval(jogo);
+      alert("Game Over :(");
+    }
+  }
+
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
@@ -66,13 +67,11 @@ function iniciarJogo(){
     food.y = Math.floor(Math.random() * 15 + 1) * box;
    }
 
-  snake.pop();
-
   let newHead = {x: snakeX,
                  y: snakeY
                 }
 
-  snake.unshift(newHead)
+  snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);
