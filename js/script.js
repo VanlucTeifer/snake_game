6ed519fd80c36f960d7e398115260d2137545d4c
Lucas Vanderlei <lucasvanderlei.f@gmail.com>
2021-02-04 08:05:30 -0300

    cria movimentos da snake

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let direction = "right";
 let snake = [];
 snake[0] = {x: 8 * box,
             y: 8 * box
@@ -17,5 +18,27 @@ function criarSnake(){
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
-criaBG();
-criarSnake();
+
+function iniciarJogo(){
+  criaBG();
+  criarSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+                  x: snakeX,
+                  y: snakeY
+                }
+
+  snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100);
